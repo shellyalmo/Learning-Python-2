e561e95a7faea9409325caafc4f7421d0d3c6987
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2019-09-18 15:38:58 +0300

    Create nested_loop.py

---
+++ nested_loop.py
@@ -0,0 +1,14 @@
+n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
+# Add your function here
+def flatten(lists):
+  results = []
+  for numbers in lists:
+    for number in numbers:
+     results.append(number)
+    
+  return results
+    
+
+
+
+print flatten(n)
