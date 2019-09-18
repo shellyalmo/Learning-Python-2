8bf97a87b6cf981c0754b5b658f272fb9492c28e
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2019-09-18 15:42:24 +0300

    nested loop.py
    """This program is demonstrating the usage of nested loops and inner and outer lists. In this program, the function gets a list that contains inner lists and loops through all of them to create a new list."""

--- nested_loop.py
+++ nested_loop.py
@@ -1,5 +1,9 @@
+"""This program is demonstrating the usage of nested loops and inner and outer lists. In this program, the function gets a list that contains inner lists and loops through all of them to create a new list."""
+
+#The outer list that contains two inner lists.
 n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
-# Add your function here
+
+#This function loops through the outer list and then through each inner list, and appends the numbers in a new list.
 def flatten(lists):
   results = []
   for numbers in lists:
@@ -8,7 +12,4 @@ def flatten(lists):
     
   return results
     
-
-
-
 print flatten(n)
